eeabe61eebb5af0ad7b8125aaa24820522c06665
Jens Reidel <adrian@travitia.xyz>
2022-12-31 05:41:59 +0100

    KenvyraLauncher: Use LineageSettings for Taskbar toggle
    Signed-off-by: Jens Reidel <adrian@travitia.xyz>

--- quickstep/src/com/android/launcher3/taskbar/TaskbarManager.java
+++ quickstep/src/com/android/launcher3/taskbar/TaskbarManager.java
@@ -57,7 +57,7 @@ import com.android.quickstep.TouchInteractionService;
 import com.android.systemui.unfold.UnfoldTransitionProgressProvider;
 import com.android.systemui.unfold.util.ScopedUnfoldTransitionProgressProvider;
 
-import android.provider.Settings;
+import lineageos.providers.LineageSettings;
 
 import java.io.PrintWriter;
 
@@ -202,7 +202,7 @@ public class TaskbarManager implements OnSharedPreferenceChangeListener {
                 SystemUiProxy.INSTANCE.get(mContext).setTaskbarEnabled(enabled);
 
                 Settings.System.putInt(mContext.getContentResolver(),
-                        Settings.System.ENABLE_TASKBAR, enabled ? 1 : 0);
+                        LineageSettings.System.ENABLE_TASKBAR, enabled ? 1 : 0);
                 break;
         }
     }
