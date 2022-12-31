a25494027fedd864eb6ff4765f5632b2c2062f80
Jens Reidel <adrian@travitia.xyz>
2022-12-31 02:00:15 +0100

    KenvyraLauncher: Rebrand
    Signed-off-by: Jens Reidel <adrian@travitia.xyz>

--- quickstep/src/com/android/launcher3/KenvyraLauncher.java
+++ quickstep/src/com/android/launcher3/KenvyraLauncher.java
@@ -18,6 +18,6 @@ package com.android.launcher3;
 
 import com.android.launcher3.uioverrides.QuickstepLauncher;
 
-public class ArrowLauncher extends QuickstepLauncher {
+public class KenvyraLauncher extends QuickstepLauncher {
 
 }
