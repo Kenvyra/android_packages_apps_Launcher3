TitaniumOS-Devices/android_frameworks_base-1

ArrowLauncher: Introduce memory info in overview
This is convenient for monitoring device status and many OEMs have
their design for this. It's time to have our own one.

mnri: forward port, refactor to T

travarilo: Adapt to QPR1 & unified navmode margin

Change-Id: I890c86c798055b1964506d9a4df1cef386562160
Signed-off-by: LibXZR <i@xzr.moe>
Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
Signed-off-by: Rizky Benggolo <travarilo@gmail.com>

quickstep/src/com/android/launcher3/uioverrides/QuickstepLauncher.java
@@ -139,6 +139,7 @@ import com.android.quickstep.util.RemoteFadeOutAnimationListener;
 import com.android.quickstep.util.SplitSelectStateController;
 import com.android.quickstep.util.TISBindHelper;
 import com.android.quickstep.util.ViewCapture;
+import com.android.quickstep.views.MemInfoView;
 import com.android.quickstep.views.OverviewActionsView;
 import com.android.quickstep.views.RecentsView;
 import com.android.quickstep.views.TaskView;
@@ -172,6 +173,7 @@ public class QuickstepLauncher extends Launcher {
     private DepthController mDepthController;
     private QuickstepTransitionManager mAppTransitionManager;
     private OverviewActionsView mActionsView;
+    private MemInfoView mMemInfoView;
     private TISBindHelper mTISBindHelper;
     private @Nullable TaskbarManager mTaskbarManager;
     private @Nullable OverviewCommandHelper mOverviewCommandHelper;
@@ -214,13 +216,16 @@ public class QuickstepLauncher extends Launcher {
         super.setupViews();
 
         mActionsView = findViewById(R.id.overview_actions_view);
+        mMemInfoView = findViewById(R.id.meminfo);
         RecentsView overviewPanel = (RecentsView) getOverviewPanel();
         SplitSelectStateController controller =
                 new SplitSelectStateController(this, mHandler, getStateManager(),
                         getDepthController(), getStatsLogManager());
-        overviewPanel.init(mActionsView, controller);
+        overviewPanel.init(mActionsView, controller, mMemInfoView);
         mActionsView.updateDimension(getDeviceProfile(), overviewPanel.getLastComputedTaskSize());
+        mMemInfoView.setDp(getDeviceProfile());
         mActionsView.updateVerticalMargin(DisplayController.getNavigationMode(this));
+        mMemInfoView.updateVerticalMargin(DisplayController.getNavigationMode(this));
 
         mAppTransitionManager = new QuickstepTransitionManager(this);
         mAppTransitionManager.registerRemoteAnimations();
@@ -735,6 +740,10 @@ public class QuickstepLauncher extends Launcher {
         return (T) mActionsView;
     }
 
+    public MemInfoView getMemInfoView () {
+        return mMemInfoView;
+    }
+
     @Override
     protected void closeOpenViews(boolean animate) {
         super.closeOpenViews(animate);
@@ -911,6 +920,9 @@ public class QuickstepLauncher extends Launcher {
             getDragLayer().recreateControllers();
             if (mActionsView != null) {
                 mActionsView.updateVerticalMargin(info.navigationMode);
+            }
+            if (mMemInfoView != null) {
+                mMemInfoView.updateVerticalMargin(info.navigationMode);
             }
         }
     }

quickstep/src/com/android/quickstep/views/MemInfoView.java
@@ -1,0 +1,150 @@
+/*
+ * Copyright (C) 2022 Project Kaleidoscope
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.quickstep.views;
+
+import android.app.ActivityManager;
+import android.content.Context;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.AttributeSet;
+import android.util.FloatProperty;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.FrameLayout.LayoutParams;
+import android.widget.TextView;
+import android.graphics.Color;
+import android.graphics.Typeface;
+
+import com.android.launcher3.anim.AlphaUpdateListener;
+import com.android.launcher3.DeviceProfile;
+import com.android.launcher3.util.MultiValueAlpha;
+import com.android.launcher3.util.NavigationMode;
+import com.android.launcher3.R;
+
+import java.lang.Runnable;
+import java.math.BigDecimal;
+
+public class MemInfoView extends TextView {
+
+    // When to show GB instead of MB
+    private static final int UNIT_CONVERT_THRESHOLD = 1024; /* MiB */
+
+    private static final BigDecimal GB2MB = new BigDecimal(1024);
+
+    private static final int ALPHA_STATE_CTRL = 0;
+    public static final int ALPHA_FS_PROGRESS = 1;
+
+    public static final FloatProperty<MemInfoView> STATE_CTRL_ALPHA =
+            new FloatProperty<MemInfoView>("state control alpha") {
+                @Override
+                public Float get(MemInfoView view) {
+                    return view.getAlpha(ALPHA_STATE_CTRL);
+                }
+
+                @Override
+                public void setValue(MemInfoView view, float v) {
+                    view.setAlpha(ALPHA_STATE_CTRL, v);
+                }
+            };
+
+    private DeviceProfile mDp;
+    private MultiValueAlpha mAlpha;
+    private ActivityManager mActivityManager;
+
+    private Handler mHandler;
+    private MemInfoWorker mWorker;
+
+    private String mMemInfoText;
+
+    public MemInfoView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        mAlpha = new MultiValueAlpha(this, 2);
+        mAlpha.setUpdateVisibility(true);
+        mActivityManager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        mHandler = new Handler(Looper.getMainLooper());
+        mWorker = new MemInfoWorker();
+
+        mMemInfoText = context.getResources().getString(R.string.meminfo_text);
+    }
+
+    /* Hijack this method to detect visibility rather than
+     * onVisibilityChanged() because the the latter one can be
+     * influenced by more factors, leading to unstable behavior. */
+    @Override
+    public void setVisibility(int visibility) {
+        super.setVisibility(visibility);
+
+        if (visibility == VISIBLE)
+            mHandler.post(mWorker);
+        else
+            mHandler.removeCallbacks(mWorker);
+    }
+
+    public void setDp(DeviceProfile dp) {
+        mDp = dp;
+    }
+
+    public void setAlpha(int alphaType, float alpha) {
+        mAlpha.getProperty(alphaType).setValue(alpha);
+    }
+
+    public float getAlpha(int alphaType) {
+        return mAlpha.getProperty(alphaType).getValue();
+    }
+
+    public void updateVerticalMargin(NavigationMode mode) {
+        LayoutParams lp = (LayoutParams)getLayoutParams();
+        int bottomMargin;
+        bottomMargin = mDp.memInfoMarginPx;
+        lp.setMargins(lp.leftMargin, lp.topMargin, lp.rightMargin, bottomMargin);
+        lp.gravity = Gravity.CENTER_HORIZONTAL | Gravity.BOTTOM;
+    }
+
+    private String unitConvert(long valueMiB, boolean alignToGB) {
+        BigDecimal rawVal = new BigDecimal(valueMiB);
+
+        if (alignToGB)
+            return rawVal.divide(GB2MB, 0, BigDecimal.ROUND_UP) + " GB";
+
+        if (valueMiB > UNIT_CONVERT_THRESHOLD)
+            return rawVal.divide(GB2MB, 1, BigDecimal.ROUND_HALF_UP) + " GB";
+        else
+            return rawVal + " MB";
+    }
+
+    private void updateMemInfoText(long availMemMiB, long totalMemMiB) {
+        String text = String.format(mMemInfoText,
+            unitConvert(availMemMiB, false), unitConvert(totalMemMiB, true));
+        setText(text);
+        setTypeface(Typeface.create("sans-serif", Typeface.NORMAL));
+        setTextColor(Color.WHITE);
+    }
+
+    private class MemInfoWorker implements Runnable {
+        @Override
+        public void run() {
+            ActivityManager.MemoryInfo memInfo = new ActivityManager.MemoryInfo();
+            mActivityManager.getMemoryInfo(memInfo);
+            long availMemMiB = memInfo.availMem / (1024 * 1024);
+            long totalMemMiB = memInfo.totalMem / (1024 * 1024);
+            updateMemInfoText(availMemMiB, totalMemMiB);
+
+            mHandler.postDelayed(this, 1000);
+        }
+    }
+}

Kenvyra/android_packages_apps_Launcher3

quickstep/src/com/android/launcher3/uioverrides/QuickstepLauncher.java
@@ -146,6 +146,7 @@ import com.android.quickstep.util.SplitToWorkspaceController;
 import com.android.quickstep.util.SplitWithKeyboardShortcutController;
 import com.android.quickstep.util.TISBindHelper;
 import com.android.quickstep.views.DesktopTaskView;
+import com.android.quickstep.views.MemInfoView;
 import com.android.quickstep.views.OverviewActionsView;
 import com.android.quickstep.views.RecentsView;
 import com.android.quickstep.views.TaskView;
@@ -181,6 +182,7 @@ public class QuickstepLauncher extends Launcher {
     private DesktopVisibilityController mDesktopVisibilityController;
     private QuickstepTransitionManager mAppTransitionManager;
     private OverviewActionsView mActionsView;
+    private MemInfoView mMemInfoView;
     private TISBindHelper mTISBindHelper;
     private @Nullable TaskbarManager mTaskbarManager;
     private @Nullable OverviewCommandHelper mOverviewCommandHelper;
@@ -231,17 +233,20 @@ public class QuickstepLauncher extends Launcher {
         super.setupViews();
 
         mActionsView = findViewById(R.id.overview_actions_view);
+        mMemInfoView = findViewById(R.id.meminfo);
         RecentsView overviewPanel = getOverviewPanel();
         mSplitSelectStateController =
                 new SplitSelectStateController(this, mHandler, getStateManager(),
                         getDepthController(), getStatsLogManager());
-        overviewPanel.init(mActionsView, mSplitSelectStateController);
+        overviewPanel.init(mActionsView, mSplitSelectStateController, mMemInfoView);
         mSplitWithKeyboardShortcutController = new SplitWithKeyboardShortcutController(this,
                 mSplitSelectStateController);
         mSplitToWorkspaceController = new SplitToWorkspaceController(this,
                 mSplitSelectStateController);
         mActionsView.updateDimension(getDeviceProfile(), overviewPanel.getLastComputedTaskSize());
+        mMemInfoView.setDp(getDeviceProfile());
         mActionsView.updateVerticalMargin(DisplayController.getNavigationMode(this));
+        mMemInfoView.updateVerticalMargin(DisplayController.getNavigationMode(this));
 
         mAppTransitionManager = new QuickstepTransitionManager(this);
         mAppTransitionManager.registerRemoteAnimations();
@@ -790,6 +795,10 @@ public class QuickstepLauncher extends Launcher {
         return (T) mActionsView;
     }
 
+    public MemInfoView getMemInfoView () {
+        return mMemInfoView;
+    }
+
     @Override
     protected void closeOpenViews(boolean animate) {
         super.closeOpenViews(animate);
@@ -980,6 +989,9 @@ public class QuickstepLauncher extends Launcher {
             getDragLayer().recreateControllers();
             if (mActionsView != null) {
                 mActionsView.updateVerticalMargin(info.navigationMode);
+            }
+            if (mMemInfoView != null) {
+                mMemInfoView.updateVerticalMargin(info.navigationMode);
             }
         }
     }

quickstep/src/com/android/quickstep/views/MemInfoView.java
@@ -1,0 +1,150 @@
+/*
+ * Copyright (C) 2022 Project Kaleidoscope
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.quickstep.views;
+
+import android.app.ActivityManager;
+import android.content.Context;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.AttributeSet;
+import android.util.FloatProperty;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.FrameLayout.LayoutParams;
+import android.widget.TextView;
+import android.graphics.Color;
+import android.graphics.Typeface;
+
+import com.android.launcher3.anim.AlphaUpdateListener;
+import com.android.launcher3.DeviceProfile;
+import com.android.launcher3.util.MultiValueAlpha;
+import com.android.launcher3.util.NavigationMode;
+import com.android.launcher3.R;
+
+import java.lang.Runnable;
+import java.math.BigDecimal;
+
+public class MemInfoView extends TextView {
+
+    // When to show GB instead of MB
+    private static final int UNIT_CONVERT_THRESHOLD = 1024; /* MiB */
+
+    private static final BigDecimal GB2MB = new BigDecimal(1024);
+
+    private static final int ALPHA_STATE_CTRL = 0;
+    public static final int ALPHA_FS_PROGRESS = 1;
+
+    public static final FloatProperty<MemInfoView> STATE_CTRL_ALPHA =
+            new FloatProperty<MemInfoView>("state control alpha") {
+                @Override
+                public Float get(MemInfoView view) {
+                    return view.getAlpha(ALPHA_STATE_CTRL);
+                }
+
+                @Override
+                public void setValue(MemInfoView view, float v) {
+                    view.setAlpha(ALPHA_STATE_CTRL, v);
+                }
+            };
+
+    private DeviceProfile mDp;
+    private MultiValueAlpha mAlpha;
+    private ActivityManager mActivityManager;
+
+    private Handler mHandler;
+    private MemInfoWorker mWorker;
+
+    private String mMemInfoText;
+
+    public MemInfoView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        mAlpha = new MultiValueAlpha(this, 2);
+        mAlpha.setUpdateVisibility(true);
+        mActivityManager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        mHandler = new Handler(Looper.getMainLooper());
+        mWorker = new MemInfoWorker();
+
+        mMemInfoText = context.getResources().getString(R.string.meminfo_text);
+    }
+
+    /* Hijack this method to detect visibility rather than
+     * onVisibilityChanged() because the the latter one can be
+     * influenced by more factors, leading to unstable behavior. */
+    @Override
+    public void setVisibility(int visibility) {
+        super.setVisibility(visibility);
+
+        if (visibility == VISIBLE)
+            mHandler.post(mWorker);
+        else
+            mHandler.removeCallbacks(mWorker);
+    }
+
+    public void setDp(DeviceProfile dp) {
+        mDp = dp;
+    }
+
+    public void setAlpha(int alphaType, float alpha) {
+        mAlpha.get(alphaType).setValue(alpha);
+    }
+
+    public float getAlpha(int alphaType) {
+        return mAlpha.get(alphaType).getValue();
+    }
+
+    public void updateVerticalMargin(NavigationMode mode) {
+        LayoutParams lp = (LayoutParams)getLayoutParams();
+        int bottomMargin;
+        bottomMargin = mDp.memInfoMarginPx;
+        lp.setMargins(lp.leftMargin, lp.topMargin, lp.rightMargin, bottomMargin);
+        lp.gravity = Gravity.CENTER_HORIZONTAL | Gravity.BOTTOM;
+    }
+
+    private String unitConvert(long valueMiB, boolean alignToGB) {
+        BigDecimal rawVal = new BigDecimal(valueMiB);
+
+        if (alignToGB)
+            return rawVal.divide(GB2MB, 0, BigDecimal.ROUND_UP) + " GB";
+
+        if (valueMiB > UNIT_CONVERT_THRESHOLD)
+            return rawVal.divide(GB2MB, 1, BigDecimal.ROUND_HALF_UP) + " GB";
+        else
+            return rawVal + " MB";
+    }
+
+    private void updateMemInfoText(long availMemMiB, long totalMemMiB) {
+        String text = String.format(mMemInfoText,
+            unitConvert(availMemMiB, false), unitConvert(totalMemMiB, true));
+        setText(text);
+        setTypeface(Typeface.create("sans-serif", Typeface.NORMAL));
+        setTextColor(Color.WHITE);
+    }
+
+    private class MemInfoWorker implements Runnable {
+        @Override
+        public void run() {
+            ActivityManager.MemoryInfo memInfo = new ActivityManager.MemoryInfo();
+            mActivityManager.getMemoryInfo(memInfo);
+            long availMemMiB = memInfo.availMem / (1024 * 1024);
+            long totalMemMiB = memInfo.totalMem / (1024 * 1024);
+            updateMemInfoText(availMemMiB, totalMemMiB);
+
+            mHandler.postDelayed(this, 1000);
+        }
+    }
+}