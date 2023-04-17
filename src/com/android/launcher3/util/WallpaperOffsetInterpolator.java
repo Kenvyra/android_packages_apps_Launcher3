TitaniumOS-Devices/android_frameworks_base-1

ArrowLauncher: Add pref to disable wallpaper scrolling
Ref:
https://github.com/LawnchairLauncher/lawnchair/commit/a969df9d3877aedcbfc0c5cdedc4b93b9b78048d

Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

src/com/android/launcher3/util/WallpaperOffsetInterpolator.java
@@ -7,6 +7,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.Message;
@@ -23,7 +24,8 @@ import com.android.launcher3.anim.Interpolators;
 /**
  * Utility class to handle wallpaper scrolling along with workspace.
  */
-public class WallpaperOffsetInterpolator extends BroadcastReceiver {
+public class WallpaperOffsetInterpolator extends BroadcastReceiver implements
+        SharedPreferences.OnSharedPreferenceChangeListener {
 
     private static final int[] sTempInt = new int[2];
     private static final String TAG = "WPOffsetInterpolator";
@@ -31,6 +33,8 @@ public class WallpaperOffsetInterpolator extends BroadcastReceiver {
 
     // Don't use all the wallpaper for parallax until you have at least this many pages
     private static final int MIN_PARALLAX_PAGE_SPAN = 4;
+
+    private static final String KEY_WALLPAPER_SCROLLING = "pref_allow_wallpaper_scrolling";
 
     private final Workspace<?> mWorkspace;
     private final boolean mIsRtl;
@@ -43,10 +47,15 @@ public class WallpaperOffsetInterpolator extends BroadcastReceiver {
     private boolean mLockedToDefaultPage;
     private int mNumScreens;
 
+    private boolean mAllowScrolling;
+
     public WallpaperOffsetInterpolator(Workspace<?> workspace) {
         mWorkspace = workspace;
         mIsRtl = Utilities.isRtl(workspace.getResources());
         mHandler = new OffsetHandler(workspace.getContext());
+        SharedPreferences prefs = Utilities.getPrefs(workspace.getContext());
+        mAllowScrolling = prefs.getBoolean(KEY_WALLPAPER_SCROLLING, true);
+        prefs.registerOnSharedPreferenceChangeListener(this);
     }
 
     /**
@@ -60,6 +69,13 @@ public class WallpaperOffsetInterpolator extends BroadcastReceiver {
         return mLockedToDefaultPage;
     }
 
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences prefs, String key) {
+        if (key.equals(KEY_WALLPAPER_SCROLLING)) {
+            mAllowScrolling = prefs.getBoolean(KEY_WALLPAPER_SCROLLING, true);
+        }
+    }
+
     /**
      * Computes the wallpaper offset as an int ratio (out[0] / out[1])
      *
@@ -70,7 +86,7 @@ public class WallpaperOffsetInterpolator extends BroadcastReceiver {
 
         // To match the default wallpaper behavior in the system, we default to either the left
         // or right edge on initialization
-        if (mLockedToDefaultPage || numScrollableScreens <= 1) {
+        if (!mAllowScrolling || mLockedToDefaultPage || numScrollableScreens <= 1) {
             out[0] =  mIsRtl ? 1 : 0;
             return;
         }

Kenvyra/android_packages_apps_Launcher3

src/com/android/launcher3/util/WallpaperOffsetInterpolator.java
@@ -7,6 +7,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.Message;
@@ -16,6 +17,7 @@ import android.view.animation.Interpolator;
 
 import androidx.annotation.AnyThread;
 
+import com.android.launcher3.LauncherPrefs;
 import com.android.launcher3.Utilities;
 import com.android.launcher3.Workspace;
 import com.android.launcher3.anim.Interpolators;
@@ -23,7 +25,8 @@ import com.android.launcher3.anim.Interpolators;
 /**
  * Utility class to handle wallpaper scrolling along with workspace.
  */
-public class WallpaperOffsetInterpolator extends BroadcastReceiver {
+public class WallpaperOffsetInterpolator extends BroadcastReceiver implements
+        SharedPreferences.OnSharedPreferenceChangeListener {
 
     private static final int[] sTempInt = new int[2];
     private static final String TAG = "WPOffsetInterpolator";
@@ -31,6 +34,8 @@ public class WallpaperOffsetInterpolator extends BroadcastReceiver {
 
     // Don't use all the wallpaper for parallax until you have at least this many pages
     private static final int MIN_PARALLAX_PAGE_SPAN = 4;
+
+    private static final String KEY_WALLPAPER_SCROLLING = "pref_allow_wallpaper_scrolling";
 
     private final Workspace<?> mWorkspace;
     private final boolean mIsRtl;
@@ -43,10 +48,15 @@ public class WallpaperOffsetInterpolator extends BroadcastReceiver {
     private boolean mLockedToDefaultPage;
     private int mNumScreens;
 
+    private boolean mAllowScrolling;
+
     public WallpaperOffsetInterpolator(Workspace<?> workspace) {
         mWorkspace = workspace;
         mIsRtl = Utilities.isRtl(workspace.getResources());
         mHandler = new OffsetHandler(workspace.getContext());
+        SharedPreferences prefs = LauncherPrefs.getPrefs(workspace.getContext());
+        mAllowScrolling = prefs.getBoolean(KEY_WALLPAPER_SCROLLING, true);
+        prefs.registerOnSharedPreferenceChangeListener(this);
     }
 
     /**
@@ -60,6 +70,13 @@ public class WallpaperOffsetInterpolator extends BroadcastReceiver {
         return mLockedToDefaultPage;
     }
 
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences prefs, String key) {
+        if (key.equals(KEY_WALLPAPER_SCROLLING)) {
+            mAllowScrolling = prefs.getBoolean(KEY_WALLPAPER_SCROLLING, true);
+        }
+    }
+
     /**
      * Computes the wallpaper offset as an int ratio (out[0] / out[1])
      *
@@ -70,7 +87,7 @@ public class WallpaperOffsetInterpolator extends BroadcastReceiver {
 
         // To match the default wallpaper behavior in the system, we default to either the left
         // or right edge on initialization
-        if (mLockedToDefaultPage || numScrollableScreens <= 1) {
+        if (!mAllowScrolling || mLockedToDefaultPage || numScrollableScreens <= 1) {
             out[0] =  mIsRtl ? 1 : 0;
             return;
         }