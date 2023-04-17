TitaniumOS-Devices/android_frameworks_base-1

ArrowLauncher: add support for custom themed icon pack [3/3]
rdEdit: forward port to 13, misc code revisions and adaptation for custom icon packs, reverted try-with-resources changes from 68af549a459aef69084ee39b0bbbdfda5cf87dcc

Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

Author: minaripenguin <minaripenguin@users.noreply.github.com>
Date:   Thu Dec 1 17:11:40 2022 +0800

    fixup! Launcher3: Add custom themed icons support

    * Follow AOSP code changes for themed icons
    * overwrite previous themed icons arraymap since users will most like change themed icon packs

    test: rename 3 custom themed icon packs to different packagenames, install, switch themed icon packs including default (system icons)

    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

Author: Rizky Benggolo <travarilo@gmail.com>
Date:   Sun Dec 18 08:24:11 2022 +0000

    Launcher3: Adapt custom themed icons to QPR1

Signed-off-by: Rizky Benggolo <travarilo@gmail.com>

src/com/android/launcher3/util/Themes.java
@@ -41,6 +41,7 @@ import com.android.launcher3.icons.GraphicsUtils;
 public class Themes {
 
     public static final String KEY_THEMED_ICONS = "themed_icons";
+    public static final String KEY_THEMED_ICON_PACK = "themed_icon_pack";
 
     public static int getActivityThemeRes(Context context) {
         final int colorHints;
@@ -74,6 +75,10 @@ public class Themes {
      */
     public static boolean isThemedIconEnabled(Context context) {
         return Utilities.getPrefs(context).getBoolean(KEY_THEMED_ICONS, false);
+    }
+
+    public static String getThemedIconPack(Context context) {
+        return Utilities.getPrefs(context).getString(KEY_THEMED_ICON_PACK, null);
     }
 
     public static String getDefaultBodyFont(Context context) {

Kenvyra/android_packages_apps_Launcher3

src/com/android/launcher3/util/Themes.java
@@ -42,6 +42,7 @@ import com.android.launcher3.icons.GraphicsUtils;
 public class Themes {
 
     public static final String KEY_THEMED_ICONS = "themed_icons";
+    public static final String KEY_THEMED_ICON_PACK = "themed_icon_pack";
 
     public static int getActivityThemeRes(Context context) {
         final int colorHints;
@@ -75,6 +76,10 @@ public class Themes {
      */
     public static boolean isThemedIconEnabled(Context context) {
         return LauncherPrefs.getPrefs(context).getBoolean(KEY_THEMED_ICONS, false);
+    }
+
+    public static String getThemedIconPack(Context context) {
+        return LauncherPrefs.getPrefs(context).getString(KEY_THEMED_ICON_PACK, null);
     }
 
     public static String getDefaultBodyFont(Context context) {